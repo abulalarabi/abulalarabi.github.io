45bc17d81c6e41ab79f9a16c773d1a5e90c0dfc7
abulalarabi <arabiabulal@gmail.com>
2022-10-05 14:40:00 -0500

    t3

---
+++ docs/precache-manifest.c7363c7256a39cf69b7d0ab41db11553.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "53936ca34bab1aa0bd06",
+    "url": "./static/css/main.047e2646.chunk.css"
+  },
+  {
+    "revision": "53936ca34bab1aa0bd06",
+    "url": "./static/js/main.991ce893.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "b3bee81f79d21461a6b2",
+    "url": "./static/js/2.d15a835d.chunk.js"
+  },
+  {
+    "revision": "e4f14a163090c58ebdbf4509d7f6c9a6",
+    "url": "./index.html"
+  }
+];
