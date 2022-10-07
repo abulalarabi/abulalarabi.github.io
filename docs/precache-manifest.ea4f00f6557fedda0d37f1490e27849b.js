33bd03045e6474e677bf0063f75818e8533050df
root <root@localhost.localdomain>
2022-10-07 13:04:44 +0000

    updates

---
+++ docs/precache-manifest.ea4f00f6557fedda0d37f1490e27849b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed827c7a17138934db54",
+    "url": "./static/css/main.df61643b.chunk.css"
+  },
+  {
+    "revision": "ed827c7a17138934db54",
+    "url": "./static/js/main.310282a3.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "b3bee81f79d21461a6b2",
+    "url": "./static/js/2.d15a835d.chunk.js"
+  },
+  {
+    "revision": "f5703db05fb6728788d9f3ad934a6ecb",
+    "url": "./static/media/google-scholar.f5703db0.svg"
+  },
+  {
+    "revision": "ac196b2d7592d4e2bed2eb25ae83c729",
+    "url": "./index.html"
+  }
+];
