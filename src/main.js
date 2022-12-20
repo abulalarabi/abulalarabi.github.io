ebf89debba7132be6e100ce14ac0f5dd7fb4cc58
Al Arabi <arabiabulal@gmail.com>
2022-12-20 14:47:51 -0600

    update

---
+++ src/main.js
@@ -0,0 +1,8 @@
+
+function aboutme(){
+    var abttxt = about.about;
+    var placeholder = document.getElementById("aboutmetext");
+    placeholder.textContent = abttxt;
+}
+
+aboutme();
---
+++ src/typist.js
@@ -0,0 +1,41 @@
+var intoinfo = document.getElementById('introline');
+
+var typewriter = new Typewriter(intoinfo, {
+  loop: false,
+  delay: 75,
+});
+
+/*
+typewriter
+  .pauseFor(2500)
+  .typeString('A simple yet powerful native javascript')
+  .pauseFor(300)
+  .deleteChars(10)
+  .typeString('<strong>JS</strong> plugin for a cool typewriter effect and ')
+  .typeString('<strong>only <span style="color: #27ae60;">5kb</span> Gzipped!</strong>')
+  .pauseFor(1000)
+  .start();
+  */
+
+typewriter
+.pauseFor(1500)
+.typeString('cat ./core.txt<br>')
+.pauseFor(500)
+.typeString('<strong>HCI and Robotics Enthusiast</strong>')
+.start();
+
+
+
+var contactinfo = document.getElementById('contactinfo');
+var typewriter2 = new Typewriter(contactinfo, {
+  loop: false,
+  delay: 75,
+});
+
+typewriter2
+.pauseFor(6000)
+.typeString('cat ./email.txt<br>')
+.pauseFor(500)
+.typeString('<strong>abulalarabi@tamu.edu<br>arabiabulal@gmail.com</strong>')
+.start();
+
